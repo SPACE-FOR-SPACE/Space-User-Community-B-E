94fe072492d91e21ed309200415219682d5bafcf
lian2945 <lian080408@gmail.com>
2024-10-02 16:43:00 +0900

    Base Commit

---
+++ src/main/java/com/example/spacecommunityuserbe/SpaceCommunityUserBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spacecommunityuserbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceCommunityUserBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceCommunityUserBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spacecommunityuserbe/SpaceCommunityUserBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spacecommunityuserbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceCommunityUserBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
